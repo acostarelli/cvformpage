71f81f5e119240460295d5c60f8adb5894bd299c
Anthony <anoky96@gmail.com>
2020-11-05 12:02:43 -0500

    twoc olumn approach isn't good

--- bootstrap/form3.js
+++ bootstrap/form3.js
@@ -1,14 +1,49 @@
 const form = document.forms[0];
 
+const toggle = (el) => {
+    if(el.style.display === "none") {
+        el.style.display = "";
+    } else {
+        el.style.display = "none";
+    }
+}
+
 form["midi-choice"].addEventListener("change", () => {
-    switch(form["midi-choice"].value) {
-        case "Custom":
-            form["stock-midi"].style.display = "none";
-            form.getElementsByClassName("custom-file")[0].style.display = "";
-            break;
-        case "Stock":
-            form["stock-midi"].style.display = "";
-            form.getElementsByClassName("custom-file")[0].style.display = "none";
-            break;
+    toggle(form["stock-midi"]);
+    toggle(form.getElementsByClassName("custom-file")[0])
+});
+
+form["parsing"].addEventListener("change", (e) => {
+    Array.from(form.getElementsByClassName("chunk-input")).map(toggle);
+    console.log(e);
+});
+
+form["method"].addEventListener("change", () => {
+    form["parsing"].value = "n-beat chunks";
+    form["parsing"].dispatchEvent(new Event("change"));
+    form["parsing"].disabled = true;
+    Array.from(form.getElementsByClassName("algo-opt")).map(e => e.style.display = "none");
+
+    const parts = form["method"].value.split(",");
+    const algo = parts[0];
+    const tags = parts.slice(1);
+
+    for(const tag of tags) {
+        switch(tag) {
+            case "pc":
+                form["parsing"].disabled = false;
+                break;
+            case "rn":
+                document.getElementById("ref-note-container").style.display = "";
+                break;
+            case "iv":
+                document.getElementById("interval-container").style.display = "";
+                break;
+            case "tr":
+                document.getElementById("trans-match-container").style.display = "";
+                document.getElementById("trans-only-container").style.display = "";
+                break;
+        }
     }
 });
+form["method"].dispatchEvent(new Event("change"));
